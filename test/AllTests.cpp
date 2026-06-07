4a1fbbc91bdfbbb34f9f8a98dbc905cf5ed7b06d
peshinam <frrr.emaeeeee@mail.ru>
2026-06-07 17:57:08 +0300

    Update AllTests.cpp

--- test/AllTests.cpp
+++ test/AllTests.cpp
@@ -1,7 +1,8 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2020 GHA Test Team
+
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
